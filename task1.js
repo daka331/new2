5d0018a05bf9f41a1156acf15155fc5c91673d46
daka331 <daria-sh89@yandex.ru>
2021-03-19 15:43:42 +0300

    Completed task1.js

--- task1.js
+++ task1.js
@@ -1,17 +1,17 @@
 const replaceString = (text, searchStr, newStr) => { //Функция должна вернуть измененный текст в котором все строки со значением searchStr заменены на newStr
-    let char = 0;
-    let result = '';
-    while (char < text.length) {
-        if (text[char] !== searchStr) {
-            return false;
-            result =`${result}${text[char].replace(searchStr, newStr)}`;
-        } else if (text[char] !== searchStr) {
-            result=`${result}${text[char]}`;
-        }
-        char += 1;
+    let result = 0;
+    while(typeof text === 'string' && typeof searchStr === 'string' && typeof newStr === 'string') {
+    if ((text.indexOf(searchStr)) === -1) {
+        result = false;
+    } else {
+    result = text.replace( new RegExp(searchStr, 'g'), newStr);
     }
     return result;
 }
-console.log(replaceString('Text text', 't', 'o'));
+return console.log("Invalid data type");
+}
+
+
+console.log(replaceString('Text text', 't', 'u'));
    
-//не работает
+
