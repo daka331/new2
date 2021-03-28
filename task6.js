55325539a284597bf7595e4d9259c94bbb1a05d4
daka331 <daria-sh89@yandex.ru>
2021-03-28 15:47:48 +0300

    task6.js but this work

--- task6.js
+++ task6.js
@@ -1,10 +1,18 @@
 //Ниже приведен код плеера, который позволяет проигрывать трек (ставить воспроизведение и паузу). Метод display выводит текущее состояние объекта. Доработайте объект таким образом, чтобы можно проигрывать плейлист и реализуйте методы next и prev, которые управляют переключением треками. Переключение треков нужно зациклить: если мы находимся на последнем треке, то при вызове next() мы попадаем на первый трек. Если мы находимся на первом треке, то при вызове prev() мы попадаем на последний трек.
 
+const playList = [
+  'song1.mp3',
+  'song2.mp3',
+  'song3.mp3',
+];
+
+let currentTrack = 0;
+
 var Player = {
     track: 'song.mp3',
     status: 'pause',
     display: function() {
-      return 'Track: ' + this.track + ' Status: ' + this.status;
+      return 'Track: ' + playList[currentTrack] + ' Status: ' + this.status;
     },
     play: function() {
       this.status = 'play';
@@ -15,27 +23,35 @@ var Player = {
     },
 
     next: function() {
-      this.status = 'play';
-      if (track = track.length) {
-        this.track = firstSong.mp3;
+      if (currentTrack < playList.length - 1) {
+        currentTrack++;
+        this.status = 'play';
       } else {
-        this.track = nextSong.mp3;
+        currentTrack = 0;
+        this.status = 'play';
       }
     },
 
     prev: function() {
-      this.status = 'play';
-      for(let i = 0; i < track.length; i++) {
-      if (track = track[0]) {
-        this.track.length;
+      if (currentTrack > 0) {
+        currentTrack--;
+        this.status = 'play';
       } else {
-        this.track --;
+        currentTrack = playList.length - 1;
+        this.status = 'play';
       }
       // TODO
     }
   };
 
-Player.display(); // "Track: song.mp3 Status: pause"
-Player.play();
-Player.display(); // "Track: song.mp3 Status: play"
+console.log(Player.display()); // "Track: song1.mp3 Status: pause"
+console.log(Player.play());
+console.log(Player.display()); // "Track: song1.mp3 Status: play"
+console.log(Player.next());
+console.log(Player.display()); // "Track: song2.mp3 Status: play"
+console.log(Player.prev());
+console.log(Player.display()); // "Track: song1.mp3 Status: play"
+console.log(Player.prev());
+console.log(Player.display()); // "Track: song3.mp3 Status: play"
+
   
